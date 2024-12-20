e65a8697ded37737fce3b12c102befd59eb0622b
Ashok2910 <ashok.anand2910@gmail.com>
2024-12-20 22:30:08 +0530

    Add files via upload

---
+++ Mockpage.Component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './MockPage.component.html',
+  styleUrls: ['./Mockpage.component.scss']
+})
+export class ContentComponent {
+  searchQuery: string = ''; //
+  categories = ['Books', 'Clothing', 'Sports', 'Home & Kitchen','Vegetables','Electronics'];
+
+  items = [
+    { 
+      name: 'Laptop', 
+      description: 'Gaming laptop', 
+      display_img_url: 'https:///', // Taken from URL
+      quantity: 3, 
+      low_quantity: 5
+    },
+    { 
+      name: 'Smartphone', 
+      description: 'Nothing 2a', 
+      display_img_url: 'https:///', // Taken from URL
+      quantity: 0, 
+      low_quantity: 5
+    },
+    { 
+      name: 'T-Shirt', 
+      description: 'V-Shaped', 
+      display_img_url: 'https:///', // Taken from URL
+      quantity: 10, 
+      low_quantity: 5
+    },
+    { 
+      name: 'Sofa', 
+      description: 'Stylish for Living room', 
+      display_img_url: '', // Empty image URL, it uses placeholder
+      quantity: 2, 
+      low_quantity: 5
+    },
+    { 
+      name: 'Headphones', 
+      description: 'Base Headphones', 
+      display_img_url: 'https:///',  // Taken from URL
+      quantity: 6, 
+      low_quantity: 5
+    }
+  ];
+
+  sortedItems = [...this.items];
+
+  sortItems() {
+    this.sortedItems.sort((a, b) => a.name.localeCompare(b.name));
+  }
+}
---
+++ app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
